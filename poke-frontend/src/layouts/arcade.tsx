d18261b4a12c4a45b8d2f9a76aaa589cbaead753
Pierre <pierre.gaillard56@orange.fr>
2025-02-20 13:08:48 +0100

    [frontend] Created Arcade shape for desktop

---
+++ poke-frontend/src/layouts/arcade.tsx
@@ -0,0 +1,36 @@
+// css
+import "./css/Arcade.css"
+
+const Arcade: React.FC = () => {
+	return (
+		<div className="arcade">
+			<div className="arcade-part arcade-front">
+				<div></div>
+			</div>
+			<div className="arcade-roof-container">
+				<div className="arcade-part arcade-roof">
+					<div></div>
+				</div>
+			</div>
+			<div className="arcade-part">
+				<div>
+					<div className="arcade-screen"></div>
+				</div>
+			</div>
+			<div className="arcade-floor-container">
+				<div className="arcade-part arcade-floor">
+					<div className="arcade-buttons">
+						<div className="button red"></div>
+						<div className="button blue"></div>
+						<div className="button yellow"></div>
+					</div>
+				</div>
+			</div>
+			<div className="arcade-part arcade-front">
+				<div></div>
+			</div>
+		</div>
+	)
+}
+
+export default Arcade
